76b1766f90bfbb58e25b5acf1aea4ae18176d32e
nageshkumargandla <nageshgandla.45@gmail.com>
2025-04-16 21:39:30 +0530

    commiting .bat file

--- run_excel_replacer.bat
+++ run_excel_replacer.bat
@@ -5,9 +5,11 @@ setlocal
 set "FILES_DIR=Files"
 set "OUTPUT_DIR=Output"
 set "JAR_FILE=target\testcasereplacer-1.0-SNAPSHOT-jar-with-dependencies.jar"
+set "RESOURCES_DIR=src\main\resources"
 
-:: Create output directory if it doesn't exist
+:: Create required directories
 if not exist "%OUTPUT_DIR%" mkdir "%OUTPUT_DIR%"
+if not exist "%RESOURCES_DIR%" mkdir "%RESOURCES_DIR%"
 
 :: Check for required files
 echo Checking files...
@@ -28,6 +30,11 @@ if not exist "%CONFIG_FILE%" (
     exit /b 1
 )
 
+:: Copy files to resources directory
+echo Copying files to resources directory...
+copy "%EXCEL_FILE%" "%RESOURCES_DIR%\IDPE Onus Tcs.xlsm" >nul
+copy "%CONFIG_FILE%" "%RESOURCES_DIR%\config.properties" >nul
+
 :: Create timestamp
 set "TIMESTAMP=%date:~-4%%date:~-7,2%%date:~-10,2%_%time:~0,2%%time:~3,2%%time:~6,2%"
 set "TIMESTAMP=%TIMESTAMP: =0%"
@@ -37,9 +44,9 @@ set "OUTPUT_FILE=%OUTPUT_DIR%\IDPE Onus Tcs_%TIMESTAMP%.xlsm"
 set "TEMP_DIR=%TEMP%\excel_replacer_%RANDOM%"
 mkdir "%TEMP_DIR%"
 
-:: Copy files
-copy "%EXCEL_FILE%" "%TEMP_DIR%\input.xlsm" >nul
-copy "%CONFIG_FILE%" "%TEMP_DIR%\config.properties" >nul
+:: Copy files to temp directory
+copy "%RESOURCES_DIR%\IDPE Onus Tcs.xlsm" "%TEMP_DIR%\input.xlsm" >nul
+copy "%RESOURCES_DIR%\config.properties" "%TEMP_DIR%\config.properties" >nul
 
 :: Run Java
 echo Running Excel Replacer...
